fd3e4b5232ef8865eaafb3fe38d17fce150b8b31
Jaroslav Klimcik <jklimcik@slido.com>
2022-09-04 14:32:30 +0200

    redeploy

--- pages/docs/[...slug].js
+++ pages/docs/[...slug].js
@@ -36,6 +36,7 @@ export async function getStaticProps({ params }) {
 
 export default function Docs({ post, prev, next }) {
   const { mdxSource, toc, frontMatter } = post
+  let test = 'test'
 
   return (
     <>
