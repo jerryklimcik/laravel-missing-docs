ec6f3cd16301330075af2ea99b5b23c9229822e9
Jaroslav Klimcik <jklimcik@slido.com>
2022-09-04 11:46:49 +0200

    polishing

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/docs', title: 'Docs' },
   { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
